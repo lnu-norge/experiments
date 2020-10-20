9133fcba6d4024c32c810f634b0d78c151f3ff3e
Marcus Alexander Dahl <marcus@programkode.studio>
2020-10-20 13:05:17 +0200

    lokaler: meta: docker setup

--- lnu.lokaler.no/programkode/frontend/src/App.tsx
+++ lnu.lokaler.no/programkode/frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Ed1t <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> 
       </header>
     </div>
   );
